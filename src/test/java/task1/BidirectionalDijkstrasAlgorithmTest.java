40127e49ef0d5c495d088503208a8440c1d4c511
coderodde <coderodd3@gmail.com>
2024-01-13 12:38:52 +0200

    Both Dijkstras agree in a brute force unit test.

--- src/test/java/task1/BidirectionalDijkstrasAlgorithmTest.java
+++ src/test/java/task1/BidirectionalDijkstrasAlgorithmTest.java
@@ -2,15 +2,26 @@ package task1;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class BidirectionalDijkstrasAlgorithmTest {
     
+    private static final int GRAPH_SIZE = 2_000;
+    private static final int ARCS = 10_000;
+    
+    private static final DijkstrasAlgorithm<DirectedGraphNode> 
+            pathfinder1 = new DijkstrasAlgorithm<>();
+    
     private static final BidirectionalDijkstrasAlgorithm<DirectedGraphNode> 
-            pathfinder = new BidirectionalDijkstrasAlgorithm<>();
+            pathfinder2 = new BidirectionalDijkstrasAlgorithm<>();
     
     private static final DirectedGraphNodeChildrenExpander childrenExpander = 
             new DirectedGraphNodeChildrenExpander();
@@ -25,7 +36,7 @@ public class BidirectionalDijkstrasAlgorithmTest {
     public void singleNodeGraphNoSelfLoop() {
         DirectedGraphNode node = new DirectedGraphNode();
         List<DirectedGraphNode> path = 
-                pathfinder.findShortestPath(
+                pathfinder2.findShortestPath(
                         node, 
                         node,
                         childrenExpander, 
@@ -43,7 +54,7 @@ public class BidirectionalDijkstrasAlgorithmTest {
         source.addChild(target, 2);
         
         List<DirectedGraphNode> path = 
-                pathfinder.findShortestPath(
+                pathfinder2.findShortestPath(
                         source, 
                         target,
                         childrenExpander, 
@@ -67,7 +78,7 @@ public class BidirectionalDijkstrasAlgorithmTest {
         target.addChild(source, -10);
         
         List<DirectedGraphNode> path = 
-                pathfinder.findShortestPath(
+                pathfinder2.findShortestPath(
                         source, 
                         target,
                         childrenExpander, 
@@ -77,6 +88,172 @@ public class BidirectionalDijkstrasAlgorithmTest {
         assertEquals(2, path.size());
         assertEquals(source, path.get(0));
         assertEquals(target, path.get(1));
+    }
+    
+    @Test
+    public void fiveNodeGraph() {
+        DirectedGraphNode s = new DirectedGraphNode();
+        DirectedGraphNode a = new DirectedGraphNode();
+        DirectedGraphNode b = new DirectedGraphNode();
+        DirectedGraphNode c = new DirectedGraphNode();
+        DirectedGraphNode t = new DirectedGraphNode();
+        
+        // Three node path:
+        s.addChild(a, 6);
+        a.addChild(t, 4);
+        
+        // Four node path (the shortest):
+        s.addChild(b, 3);
+        b.addChild(c, 3);
+        c.addChild(t, 3);
+        
+        List<DirectedGraphNode> path = 
+                pathfinder2.findShortestPath(
+                        s, 
+                        t,
+                        childrenExpander, 
+                        parentsExpander,
+                        weightFunction);
+        
+        assertEquals(4, path.size());
+        assertEquals(s, path.get(0));
+        assertEquals(b, path.get(1));
+        assertEquals(c, path.get(2));
+        assertEquals(t, path.get(3));
+    }
+    
+    @Test
+    public void bruteForceComparisonToDijkstra() {
+        Random random = new Random(13L);
+        
+        DirectedGraphNodeChildrenExpander childrenExpander =
+                new DirectedGraphNodeChildrenExpander();
+        
+        DirectedGraphNodeParentsExpander parentsExpander = 
+                new DirectedGraphNodeParentsExpander();
+        
+        for (int i = 0; i < 10; i++) {
+            GraphData graphData = getRandomGraph(GRAPH_SIZE, ARCS, random);
+            DirectedGraphWeightFunction weightFunction = 
+                    graphData.getWeightFunction();
+            
+            DirectedGraphNode source = graphData.getRandonNode(random);
+            DirectedGraphNode target = graphData.getRandonNode(random);
+            
+            List<DirectedGraphNode> path1 = null;
+            List<DirectedGraphNode> path2 = null;
+            
+            try {
+                path1 = pathfinder1.findShortestPath(
+                        source, 
+                        target, 
+                        childrenExpander, 
+                        weightFunction);
+            } catch (IllegalStateException ex) {
+                
+            }
+            
+            try {
+                path2 = pathfinder2.findShortestPath(
+                        source, 
+                        target,
+                        childrenExpander, 
+                        parentsExpander,
+                        weightFunction);
+            } catch (IllegalStateException ex) {
+                
+            }
+            
+            assertTrue(Objects.equals(path1, path2));
+        }
+    }
+    
+    private static final class GraphData {
+        private final List<DirectedGraphNode> graphNodes;
+        private final DirectedGraphWeightFunction weightFunction;
+        
+        GraphData(List<DirectedGraphNode> graphNodes,
+                  DirectedGraphWeightFunction weightFunction) {
+            
+            this.graphNodes = graphNodes;
+            this.weightFunction = weightFunction;
+        }
+        
+        List<DirectedGraphNode> getGraphNodes() {
+            return graphNodes;
+        }
+        
+        DirectedGraphWeightFunction getWeightFunction() {
+            return weightFunction;
+        }
+        
+        DirectedGraphNode getRandonNode(Random random) {
+            return choose(graphNodes, random);
+        }
+    }
+    
+    private static final GraphData 
+        getRandomGraph(int nodes, int edges, Random random) {
+        
+        List<DirectedGraphNode> graph = new ArrayList<>(nodes);
+        Set<Arc> arcs = new HashSet<>(edges);
+        
+        for (int i = 0; i < nodes; i++) {
+            graph.add(new DirectedGraphNode());
+        }
+        
+        while (arcs.size() < edges) {
+            DirectedGraphNode tail = choose(graph, random);
+            DirectedGraphNode head = choose(graph, random);
+            Arc arc = new Arc(tail, head);
+            arcs.add(arc);
+        }
+        
+        DirectedGraphWeightFunction weightFunction = 
+                new DirectedGraphWeightFunction();
+        
+        for (Arc arc : arcs) {
+            DirectedGraphNode tail = arc.getTail();
+            DirectedGraphNode head = arc.getHead();
+            int weight = random.nextInt(100);
+            tail.addChild(head, weight);
+        }
+        
+        return new GraphData(graph, weightFunction);
+    }
+        
+    private static <T> T choose(List<T> list, Random random) {
+        return list.get(random.nextInt(list.size()));
+    }
+        
+    private static final class Arc {
+        private final DirectedGraphNode tail;
+        private final DirectedGraphNode head;
+        
+        Arc(DirectedGraphNode tail, DirectedGraphNode head) {
+            this.tail = tail;
+            this.head = head;
+        }
+        
+        DirectedGraphNode getTail() {
+            return tail;
+        }
+        
+        DirectedGraphNode getHead() {
+            return head;
+        }
+        
+        @Override
+        public int hashCode() {
+            return Objects.hash(tail, head);
+        }
+        
+        @Override
+        public boolean equals(Object o) {
+            Arc arc = (Arc) o;
+            return tail.equals(arc.tail) && 
+                   head.equals(arc.head);
+        }
     }
 }
 
@@ -91,6 +268,10 @@ final class DirectedGraphNode {
     
     DirectedGraphNode() {
         this.id = nodeIdCounter++;
+    }
+    
+    int getId() {
+        return id;
     }
     
     void addChild(DirectedGraphNode child, int weight) {
