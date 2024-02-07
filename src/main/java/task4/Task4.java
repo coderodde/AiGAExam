cde4a198c5eac7814e103492482c7c9f18f1439b
Rodion Efremov <coderodd3@yandex.com>
2024-02-07 20:04:13 +0200

    Demo program prints time statistics.

--- src/main/java/task4/Task4.java
+++ src/main/java/task4/Task4.java
@@ -18,7 +18,8 @@ public class Task4 {
     private static SemiDynamicRMQTree<?, Integer, Long> 
         constructREPLTree(int size) {
             
-        Set<KeyValuePair<Integer, Long>> keyValuePairSet = new HashSet<>();
+        long start = System.nanoTime();
+        Set<KeyValuePair<Integer, Long>> keyValuePairSet = new HashSet<>(size);
         
         for (int i = 0; i < size; i++) {
             Integer key = i + 1;
@@ -27,7 +28,30 @@ public class Task4 {
             keyValuePairSet.add(keyValuePair);
         }
         
-        return new SemiDynamicRMQTree<>(keyValuePairSet);
+        long end = System.nanoTime();
+        long total = end - start;
+        
+        System.out.printf(
+                "Built the key/value pairs in %,d nanoseconds.\n",
+                total);
+        
+        start = System.nanoTime();
+        
+        SemiDynamicRMQTree<?, Integer, Long> tree =
+                new SemiDynamicRMQTree<>(keyValuePairSet);
+
+        end = System.nanoTime();
+        
+        System.out.printf("Built the RMQ tree in %,d nanoseconds.\n", 
+                          end - start);
+        
+        total += end - start;
+        
+        System.out.printf(
+                "Total time building the RMQ tree: %,d nanoseconds.\n",
+                total);
+        
+        return tree;
     }
              
     private static void replInterface() {
@@ -94,12 +118,19 @@ public class Task4 {
     private static void runUpdate(String keyString, String newValueString) {
         Integer key = Integer.valueOf(keyString);
         Long value = Long.valueOf(newValueString);
+        long start = System.nanoTime();
         tree.update(key, value);
+        long end = System.nanoTime();
+        System.out.printf("update in %,d nanoseconds.\n", end - start);
     }
     
     private static Long runRMQ(String leftKeyString, String rightKeyString) {
         Integer leftKey = Integer.valueOf(leftKeyString);
         Integer rightKey = Integer.valueOf(rightKeyString);
-        return tree.getRangeMinimum(leftKey, rightKey);
+        long start = System.nanoTime();
+        Long returnValue = tree.getRangeMinimum(leftKey, rightKey);
+        long end = System.nanoTime();
+        System.out.printf("rmq in %,d nanoseconds.\n", end - start);
+        return returnValue;
     }
 }
