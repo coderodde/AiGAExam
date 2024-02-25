12765f2e02b3b3c3db74829ff6dede0dbc311d78
Rodion Efremov <coderodd3@yandex.com>
2024-02-25 11:52:11 +0200

    Added more beef to the HMM demo.

--- src/main/java/task6/Task6.java
+++ src/main/java/task6/Task6.java
@@ -2,6 +2,7 @@ package task6;
 
 import java.util.List;
 import java.util.Random;
+import static task6.HiddenMarkovModel.sumPathProbabilities;
 
 /**
  * This class implements the demonstration of the hidden Markov model.
@@ -101,5 +102,82 @@ public final class Task6 {
         for (HiddenMarkovModelStatePath stateSequence : statePathSequences) {
             System.out.printf("%4d: %s\n", lineNumber++, stateSequence);
         }
+        
+        System.out.println("--- TEST ---");
+        
+        for (int i = 0; i < 5; i++) {
+            String sequence2 = buildRandomSequence(random);
+            
+            System.out.printf("Sequence: \"%s\":", sequence2);
+            
+            long start = System.nanoTime();
+            
+            List<HiddenMarkovModelStatePath> dfsPaths =
+                    hmm.computeAllStatePaths(sequence2);
+            
+            long end = System.nanoTime();
+            
+            System.out.printf("  All paths in %,d nanoseconds:\n", end - start);
+            
+            for (HiddenMarkovModelStatePath path : dfsPaths) {
+                System.out.printf("    %s.\n", path);
+            }
+            
+            System.out.printf("  Total probability:     %.3f.\n", 
+                              sumPathProbabilities(dfsPaths));
+            
+            start = System.nanoTime();
+            
+            double p = hmm.runForwardAlgorithm(sequence2);
+            
+            end = System.nanoTime();
+            
+            System.out.printf("  HMM total probability: %.3f, duration: %,d nanoseconds.\n",
+                              p,
+                              end - start);
+            
+            System.out.printf("  Most probable brute-force path:  %s.\n",
+                              dfsPaths.get(0));
+            
+            start = System.nanoTime();
+            
+            HiddenMarkovModelStatePath viterbiPath =
+                    hmm.runViterbiAlgorithm(sequence2);
+            
+            end = System.nanoTime();
+            
+            System.out.printf("  Viterbi path %s in %,d nanoseconds.\n", 
+                              viterbiPath,
+                              end - start);
+        }
+    }
+    
+    private static String buildRandomSequence(Random random) {
+        int sequenceLength = 1 + random.nextInt(10);
+        StringBuilder sb = new StringBuilder(sequenceLength);
+        
+        for (int i = 0; i < sequenceLength; i++) {
+            sb.append(getRandomCharacter(random));
+        }
+        
+        return sb.toString();
+    }
+    
+    private static char getRandomCharacter(Random random) {
+        double coin = random.nextDouble();
+        
+        if (coin < 0.25) {
+            return 'A';
+        }
+        
+        if (coin < 0.5) {
+            return 'T';
+        }
+        
+        if (coin < 0.75) {
+            return 'C';
+        }
+        
+        return 'G';
     }
 }
